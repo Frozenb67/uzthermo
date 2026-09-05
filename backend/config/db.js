cbd7766150cfb0d0a407bddf45c324950d9cbf02
Nurmuhammad <nurmuhammadmirsamatov@gmail.com>
2026-09-05 20:01:44 +0500

    sd

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect("mongodb+srv://user:user123@cluster0.njzp0pk.mongodb.net/?appName=Cluster0");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
